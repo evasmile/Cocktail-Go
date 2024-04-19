8502302a737e6e3d5f6dac1b20e408161f783e38
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-04-19 21:45:35 +0200

    add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import express from'express'
+import express from'express';
 import bodyParser from "body-parser";
+import axios from 'axios';
 
 
 
@@ -9,8 +10,21 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-app.get('/',function(req,res){
-    res.render('home.ejs')
+app.get('/',async function  (req,res){
+let result1 = null ;
+let result2 = null;
+ try{
+    const response1 = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const response2 = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+     result1 = response1.data
+     result2 = response2.data
+    console.log(result1)
+
+ }catch(error){
+   console.log(error)
+ }
+
+    res.render('home.ejs',{data1:result1,data2:result2})
 })
 
 
